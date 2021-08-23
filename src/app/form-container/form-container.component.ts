e5dc03527ddb9b454182549ffe36e7c42c0d05a4
Muktadir Imam Jan <muktadirimam@gmail.com>
2021-08-23 19:13:52 +0600

    reusable component partially done

--- src/app/form-container/form-container.component.ts
+++ src/app/form-container/form-container.component.ts
@@ -1,39 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators, FormBuilder} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
-
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
-
+import { Validators, FormBuilder } from '@angular/forms';
 @Component({
-  selector: 'app-form-container',
-  templateUrl: './form-container.component.html',
-  styleUrls: ['./form-container.component.scss']
+    selector: 'app-form-container',
+    templateUrl: './form-container.component.html',
+    styleUrls: ['./form-container.component.scss'],
 })
 export class FormContainerComponent implements OnInit {
+    constructor(private fb: FormBuilder) {}
 
-  constructor( private fb: FormBuilder) { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+    fromGroup = {
+        emailFormControl: ['', [Validators.required, Validators.email]],
+        passFormControl: [null, Validators.required],
+        phoneFormControl: [null, Validators.required],
+    };
 
-  fromGroup={
-    emailFormControl:['', [Validators.required, Validators.email]],
-    passFormControl:[null, Validators.required],
-    phoneFormControl:[null, Validators.required],
-  }
+    form = this.fb.group(this.fromGroup);
 
-  form = this.fb.group(this.fromGroup)
-
-
-
-  matcher = new MyErrorStateMatcher();
-
-  onSubmit(){
-      console.log(this.form.value);
-  }
+    onSubmit() {
+        console.log(this.form, this.form.value);
+    }
 }
--- src/app/form-container/input-component/input-component.component.ts
+++ src/app/form-container/input-component/input-component.component.ts
@@ -1,58 +1,104 @@
-import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {
     AbstractControl,
     ControlValueAccessor,
+    FormControl,
+    FormGroup,
     NG_VALIDATORS,
     NG_VALUE_ACCESSOR,
     ValidationErrors,
-    Validator
+    ValidatorFn,
+    Validators,
 } from '@angular/forms';
 
-@Component({
-  selector: 'app-input-component',
-  templateUrl: './input-component.component.html',
-  styleUrls: ['./input-component.component.scss'],
-  providers:[
-      {
-          provide: NG_VALUE_ACCESSOR,
-          useExisting: InputComponentComponent,
-          multi: true
-      },
-      {
-        provide: NG_VALIDATORS,
-        useExisting: InputComponentComponent,
-        multi: true,
-      },
-  ]
-})
-export class InputComponentComponent implements ControlValueAccessor, Validator {
-
-  @Input() type = 'text';
-  @Input() isRequired: boolean = false;
-  @Input() pattern = null;
-  @Input() label: string = 'label';
-  @Input()
-    placeholder!: string;
-  @Input() errorMsg: any;
-  @Input()
-    matcher!: () => void;
-
-   public valueVar!: string;
-   public onChange!: (val: any) => void;
-   public onTouched!: () => void;
-
-  constructor() {}
-
-    isValid(val: any) {
-        const x = 0;
+const isValidPass = (val: string) => {
+    let re = /[0-9]/;
+    if (!re.test(val)) {
         return false;
     }
+    re = /[a-z]/;
+    if (!re.test(val)) {
+        return false;
+    }
+    re = /[A-Z]/;
+    if (!re.test(val)) {
+        return false;
+    }
+    if (val.length <= 8) {
+        return false;
+    }
+    return true;
+};
 
-    validate(control: AbstractControl): ValidationErrors | null {
-        return control.value && this.isValid(control.value) ? null : { val: true };
+function forbiddenNameValidator(type: string): ValidatorFn {
+
+    return (control: AbstractControl): ValidationErrors | null => {
+        const val = control.value;
+        let forbidden = false;
+        switch (type) {
+            case 'email':
+                forbidden = val.includes('.com');
+                return forbidden ? null:{email: true};
+            case 'password':
+                forbidden = isValidPass(val);
+                return forbidden ? null:{pass: true};
+            case 'tel':
+                forbidden = true;
+                return forbidden ? null:{phone: true};
+
+            case 'text':
+            default:
+                return forbidden ? {value: true}: null;
+        }
+    };
+  }
+
+@Component({
+    selector: 'app-input-component',
+    templateUrl: './input-component.component.html',
+    styleUrls: ['./input-component.component.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: InputComponentComponent,
+            multi: true,
+        },
+        {
+            provide: NG_VALIDATORS,
+            useExisting: InputComponentComponent,
+            multi: true,
+        },
+    ],
+})
+
+export class InputComponentComponent
+    implements ControlValueAccessor, OnInit
+{
+    @Input() type = 'text';
+    @Input() label: string = 'label';
+    @Input() placeholder!: string;
+    @Input() errorMsg: any;
+
+    public valueVar!: string;
+    public onChange!: (val: any) => void;
+    public onTouched!: () => void;
+    myGroup!: FormGroup;
+
+    constructor() {}
+
+    ngOnInit(){
+        this.myGroup = new FormGroup({
+            inputVal: new FormControl('', [Validators.required, forbiddenNameValidator(this.type)])
+         });
     }
 
+    get formControlVal() {
+        return this.myGroup.controls.inputVal;
+      }
 
+    validate(_: FormControl) {
+        return this.myGroup.valid ? null : { [this.type]: { valid: false } };
+      }
 
     writeValue(obj: any): void {
         this.valueVar = obj;
@@ -63,24 +109,14 @@ export class InputComponentComponent implements ControlValueAccessor, Validator 
     }
 
     registerOnTouched(fn: any): void {
-      this.onTouched = fn
+        this.onTouched = fn;
     }
 
-    setDisabledState(isDisable: boolean): void {
+    setDisabledState(isDisable: boolean): void {}
 
-    }
-
-    setValue(ev:any){
+    setValue(ev: any) {
         this.valueVar = ev.target.value;
         this.onChange(this.valueVar);
     }
-
-    // onChange(event: any){
-    //   console.log(event);
-    // }
-    // onTouched(){
-    //     console.log("touched");
-
-    // }
 
 }
